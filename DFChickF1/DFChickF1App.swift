192bf357832fb4c8fdb1ed5e4a217f11c2d8cbc6
DJUROM777 <djurom777@ya.ru>
2025-08-08 12:07:43 +0300

    Initial Commit

---
+++ DFChickF1/DFChickF1App.swift
@@ -0,0 +1,17 @@
+//
+//  DFChickF1App.swift
+//  DFChickF1
+//
+//  Created by IGOR on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct DFChickF1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
